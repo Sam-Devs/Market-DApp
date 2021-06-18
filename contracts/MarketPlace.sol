627a09070005a930ebbd514685ae12571d1067de
Sam Ayorinde <41350405+Sam-Devs@users.noreply.github.com>
2021-06-18 15:15:53 +0100

    contract

--- contracts/MarketPlace.sol
+++ contracts/MarketPlace.sol
@@ -1,1 +1,20 @@
 pragma solidity 0.6.12;
+
+contract MarketPlace {
+    enum Status {PENDING, SOLD }
+    struct Offer {
+        uint id;
+        string memory description;
+        uint price;
+        address seller;
+    }
+
+    struct Trade {
+        uint id;
+        address buyer;
+        address seller;
+        uint price;
+        uint offerId;
+        Status status;
+    }
+}

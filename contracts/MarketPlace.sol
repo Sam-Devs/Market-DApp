142ebf49a9c92dd1b5a93f1392eec9db5ec5ba62
Sam Ayorinde <41350405+Sam-Devs@users.noreply.github.com>
2021-06-18 15:47:16 +0100

    finalized contract

--- contracts/MarketPlace.sol
+++ contracts/MarketPlace.sol
@@ -18,10 +18,88 @@ contract MarketPlace {
         Status status;
     }
 
+    unit offerID = 0;
+    uint tradeID = 0;
+
     mapping (uint => Trade) public trades;
     mapping (uint => Offer) public offers;
     mapping(address => uint) public balances;
     mapping(address => uint) public availableBalances;
     mapping(address => bool) public members;
     address admin;
+
+    constructor() {
+        admin = msg.sender;
+    }
+
+    modifier() {
+        require(msg.sender == admin, "Only Admin")
+        _;
+    }
+
+    modifier(bool registered, address member) {
+        if(registered) {
+            require(members[member] == true, "must be registered");
+        } else {
+            require(members[member] == false, "must not be registered");
+        }
+        _;
+    }
+
+    function buy(uint offerId) external onlyMember(msg.sender, true) {
+        Offer storage offer = offers[offerId];
+        require(offer.id > 0, 'offer must exist');
+        require(offer.status == Status.PENDING, 'offer must be pending');
+        require(availableBalances[msg.sender] >= offer.price);
+        availableBalances[msg.sender] -= offer.price;
+        offer.status = Status.DONE;
+        uint tradeId = lastTradeId++;
+        trades[lastTradeId++] = Trade(tradeId, offer.id, msg.sender, offer.seller, offer.price, Status.PENDING);
+    }
+
+    function deposit() 
+        external 
+        payable 
+        onlyMember(msg.sender, true) {
+        balances[msg.sender] += msg.value;
+        availableBalances[msg.sender] += msg.value;
+    }
+    
+    /*
+     * Admin functions
+     */
+    
+    function settle(uint txId) 
+        external 
+        onlyAdmin() {
+        Trade storage trade = trades[txId];
+        require(trade.id != 0, 'trade must exist');
+        require(trade.status == Status.PENDING, 'trade must be in PENDING state');
+        trade.status = Status.DONE;
+        availableBalances[msg.sender] += trade.price;
+        _transfer(trade.buyer, trade.seller, trade.price);
+    }
+    
+    function register(address member) 
+        external 
+        onlyMember(member, false)
+        onlyAdmin() {
+        members[member] = true;
+        balances[member] += 500;
+        availableBalances[member] += 500;
+    }
+    
+    function unregister(address member) external onlyMember(member, true) onlyAdmin() {
+        uint memberBalance = balances[member];
+        members[member] = false;
+        _transfer(member, address(this), memberBalance);
+    }
+    
+    function _transfer(address from, address to, uint amount) internal {
+        require(balances[from] >= amount, 'cannot transfer more than current balance');
+        balances[from] -= amount;
+        availableBalances[from] -= amount;
+        balances[to] += amount;
+        availableBalances[to] += amount;
+    }
 }
